58a38d876d033645fbd5ba7c32aa661a13038a3c
Mohammadjavad Aghaeipour <mohammadjavad.a.k85@gmail.com>
2025-09-30 18:15:21 -0400

    changes made to the calculator

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -14,7 +14,7 @@ import java.math.BigDecimal;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button btn1, btn2, btn_clear, btn_plus, btn_equal;
+    Button btn0, btn1, btn2, btn3, btn4 ,btn5 ,btn6 , btn7, btn8, btn9, btn_clear, btn_plus, btn_minos, btn_multiply, btn_devide, btn_equal, btn_dot;
     TextView text_display;
 
     // This is to evaluate the math expression
@@ -25,9 +25,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
 
+        btn0 = (Button) findViewById(R.id.btn0);
         btn1 = (Button) findViewById(R.id.btn1);
         btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+        btn4 = (Button) findViewById(R.id.btn4);
+        btn5 = (Button) findViewById(R.id.btn5);
+        btn6 = (Button) findViewById(R.id.btn6);
+        btn7 = (Button) findViewById(R.id.btn7);
+        btn8 = (Button) findViewById(R.id.btn8);
+        btn9 = (Button) findViewById(R.id.btn9);
+        btn_dot = (Button) findViewById(R.id.btn_dot);
         btn_plus = (Button) findViewById(R.id.btn_plus);
+        btn_minos = (Button) findViewById(R.id.btn_minos);
+        btn_multiply = (Button) findViewById(R.id.btn_multiply);
+        btn_devide = (Button) findViewById(R.id.btn_devide);
         btn_equal = (Button) findViewById(R.id.btn_equal);
         btn_clear = (Button) findViewById(R.id.btn_clear);
         text_display = (TextView) findViewById(R.id.textview_input_display);
@@ -36,24 +48,69 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void setClickListeners() {
+        btn0.setOnClickListener(this);
         btn1.setOnClickListener(this);
         btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+        btn6.setOnClickListener(this);
+        btn7.setOnClickListener(this);
+        btn8.setOnClickListener(this);
+        btn9.setOnClickListener(this);
         btn_plus.setOnClickListener(this);
+        btn_minos.setOnClickListener(this);
+        btn_multiply.setOnClickListener(this);
+        btn_devide.setOnClickListener(this);
         btn_equal.setOnClickListener(this);
         btn_clear.setOnClickListener(this);
+        btn_dot.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+            case R.id.btn0:
+                addNumber("0");
+                break;
             case R.id.btn1:
                 addNumber("1");
                 break;
             case R.id.btn2:
                 addNumber("2");
                 break;
+            case R.id.btn3:
+                addNumber("3");
+                break;
+            case R.id.btn4:
+                addNumber("4");
+                break;
+            case R.id.btn5:
+                addNumber("5");
+                break;
+            case R.id.btn6:
+                addNumber("6");
+                break;
+            case R.id.btn7:
+                addNumber("7");
+                break;
+            case R.id.btn8:
+                addNumber("8");
+                break;
+            case R.id.btn9:
+                addNumber("9");
+                break;
             case R.id.btn_plus:
                 addNumber("+");
+                break;
+            case R.id.btn_minos:
+                addNumber("-");
+                break;
+            case R.id.btn_multiply:
+                addNumber("×");
+                break;
+            case R.id.btn_devide:
+                addNumber("÷");
                 break;
             case R.id.btn_equal:
                 String result = null;
@@ -67,14 +124,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_clear:
                 clear_display();
                 break;
+            case R.id.btn_dot:
+                addNumber(".");
+                break;
         }
     }
 
-    public static String evaluate(String expression) throws Exception {
-        String result = evaluate(expression);
+    private String evaluate(String expression) throws Exception {
+        //to go from frontend form for multiplication and division to backend https://www.w3schools.com/jsref/jsref_replace.asp
+        String Expr = expression.replace("×", "*").replace("÷", "/");
+
+        String result = MathEval.eval(Expr);
+
         BigDecimal decimal = new BigDecimal(result);
-        return decimal.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString();
+
+        if (decimal.scale() > 0 && decimal.stripTrailingZeros().scale() > 0) {
+            return decimal.stripTrailingZeros().toPlainString();
+        } else {
+            return decimal.toBigInteger().toString();
+        }
     }
+
+
 
     private void addNumber(String number) {
         text_display.setText(text_display.getText() + number);
--- app/src/test/java/com/example/mycalculator/ExampleUnitTest.java
+++ app/src/test/java/com/example/mycalculator/ExampleUnitTest.java
@@ -1,5 +1,7 @@
 package com.example.mycalculator;
+
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
@@ -10,70 +12,6 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
-        String twoPlusTwo = MathEval.eval("2+2");
-        assertEquals("4", twoPlusTwo);
-
-        String decimalAddition = MathEval.eval("6.4+7.3");
-        assertEquals("13.7", decimalAddition);
-
-        String decimalWholeAdd = MathEval.eval("2.5+2.5");
-        assertEquals("5", decimalWholeAdd);
-
-        String negativeAddition = MathEval.eval("-4+7");
-        assertEquals("3", negativeAddition);
-
-        String negativeResult = MathEval.eval("-10+-2");
-        assertEquals("-12", negativeResult);
-    }
-
-    public void subtraction_isCorrect() {
-        String regularInts = MathEval.eval("5-3");
-        assertEquals("2", regularInts);
-        String decimalSubtraction = MathEval.eval("11-6.75");
-        assertEquals("4.25", decimalSubtraction);
-        String decimalWholeSubtraction = MathEval.eval("7.5-4.5");
-        assertEquals("3", decimalWholeSubtraction);
-        String negativeResult = MathEval.eval("4-8");
-        assertEquals("-4", negativeResult);
-    }
-
-    public void multiplication_isCorrect() {
-        String regularMultiplication = MathEval.eval("5*4");
-        assertEquals("20", regularMultiplication);
-
-        String decimalMultiplication = MathEval.eval("6*1.5");
-        assertEquals("9", decimalMultiplication);
-
-        String decimalResult = MathEval.eval("2.5*3");
-        assertEquals("7.5", decimalResult);
-
-        String negativeMultiplication = MathEval.eval("-8*5");
-        assertEquals("-40", negativeMultiplication);
-
-        String doubleNegative = MathEval.eval("-3*-9");
-        assertEquals("27", doubleNegative);
-
-        String negativeAndDecimal = MathEval.eval("-7*2.5");
-        assertEquals("-17.5", negativeAndDecimal);
-    }
-
-    public void division_isCorrect() {
-        String regularDivision = MathEval.eval("6/3");
-        assertEquals("2", regularDivision);
-
-        String decimalDivision = MathEval.eval("12/0.5");
-        assertEquals("24", decimalDivision);
-
-        String decimalResult = MathEval.eval("9/2");
-        assertEquals("4.5", decimalResult);
-
-        String negativeResult = MathEval.eval("8/-4");
-        assertEquals("-2", negativeResult);
-
-        String doubleNegative = MathEval.eval("-9/-3");
-        assertEquals("3", doubleNegative);
-
-        String negativeAndDecimal = MathEval.eval("-2/-0.25");
-        assertEquals("8", negativeAndDecimal);
+        assertEquals(4, 2 + 2);
     }
 }
